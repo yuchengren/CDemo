5af28996a4c55d3ab10927f8f5340d661726a7cc
yuchengren <513082359@qq.com>
2019-07-14 21:59:47 +0800

    CDemo，初始化提交

---
+++ cmake-build-debug/CMakeFiles/CDemo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CDemo.dir/main.c.o"
+  "CDemo.pdb"
+  "CDemo.exe"
+  "libCDemo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
